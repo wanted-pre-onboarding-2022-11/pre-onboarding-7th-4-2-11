6608df76ef22bf9865a35ecb5f8df20e80f4807a
0SCAR0421 <oscar0421@kakao.com>
2022-11-15 22:27:22 +0900

    Feat: localhost util 함수 작성

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,31 @@
+export const ACCESS_TOKEN_KEY = "accessToken";
+export const USER_DATA = "userData";
+
+export const saveFetchData = (
+  accessToken: string,
+  userData: { id: number; email: string },
+): void => {
+  localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+  localStorage.setItem(USER_DATA, JSON.stringify(userData));
+};
+
+export const getAccessToken = (): null | string => {
+  return localStorage.getItem(ACCESS_TOKEN_KEY);
+};
+
+export const getUserName = (): string => {
+  const temp = localStorage.getItem(USER_DATA);
+
+  if (temp === null) {
+    return "ERROR";
+  }
+
+  const userData = JSON.parse(temp).email;
+
+  return userData.split("@")[0];
+};
+
+export const deleteFetchData = (): void => {
+  localStorage.removeItem(ACCESS_TOKEN_KEY);
+  localStorage.removeItem(USER_DATA);
+};
