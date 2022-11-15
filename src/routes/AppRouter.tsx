854fe609d35e138d3b336b33c98ec49637a412ac
0SCAR0421 <oscar0421@kakao.com>
2022-11-15 22:25:58 +0900

    Feat: router 설정 추가 및 변경

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ROUTE_PATH from "./routerPaths";
+import MainPage from "../pages/MainPage";
+import LoginPage from "../pages/LoginPage";
+import DetailPage from "../pages/DetailPage";
+import ProtectedRoute from "./AuthorizationRouter";
+import Layout from "../components/Layout/Layout";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={ROUTE_PATH.LOGIN}
+          element={
+            <ProtectedRoute isTokenRequired redirectPath={ROUTE_PATH.BASE}>
+              <LoginPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route element={<Layout />}>
+          <Route
+            path={ROUTE_PATH.BASE}
+            element={
+              <ProtectedRoute isTokenRequired={false} redirectPath={ROUTE_PATH.LOGIN}>
+                <MainPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path={`${ROUTE_PATH.DETAIL}/:id`}
+            element={
+              <ProtectedRoute isTokenRequired={false} redirectPath={ROUTE_PATH.LOGIN}>
+                <DetailPage />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+        <Route path={ROUTE_PATH.NOT_FOUND} element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
---
+++ src/routes/routerPaths.ts
@@ -0,0 +1,8 @@
+const ROUTE_PATH = {
+  BASE: "/",
+  DETAIL: "/detail",
+  LOGIN: "/login",
+  NOT_FOUND: "*",
+};
+
+export default ROUTE_PATH;
