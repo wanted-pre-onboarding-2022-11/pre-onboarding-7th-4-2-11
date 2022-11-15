535704b1d80547747622e73e5221a7c90a63889f
0SCAR0421 <oscar0421@kakao.com>
2022-11-15 22:33:38 +0900

    Feat: Footer 컴포넌트 작성

---
+++ src/components/Layout/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-center p-5 border-t">
+      copyright © December and Company Inc.
+    </div>
+  );
+};
+
+export default Footer;
