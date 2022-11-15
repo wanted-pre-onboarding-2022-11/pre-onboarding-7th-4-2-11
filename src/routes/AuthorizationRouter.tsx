b417f39aca698d67de496560ad1339fe18adc958
0SCAR0421 <oscar0421@kakao.com>
2022-11-15 22:29:01 +0900

    Feat: token 인증 로직 컴포넌트  작성

---
+++ src/routes/AuthorizationRouter.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { getAccessToken } from "../utils/localStorage";
+
+const ProtectedRoute = ({
+  children,
+  isTokenRequired = true,
+  redirectPath,
+}: {
+  children: JSX.Element;
+  isTokenRequired: boolean;
+  redirectPath: string;
+}) => {
+  const token = getAccessToken();
+  const location = useLocation();
+
+  if (isTokenRequired === (token !== null))
+    return <Navigate to={redirectPath} state={{ from: location }} replace />;
+  return children;
+};
+
+export default ProtectedRoute;
