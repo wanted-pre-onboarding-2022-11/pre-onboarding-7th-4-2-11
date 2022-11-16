0ea1d375b39bf0c5ce35db5144939fa0b008b7a8
0SCAR0421 <oscar0421@kakao.com>
2022-11-16 23:32:09 +0900

    Docs: AccountList, AccountProps 타입정의

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,23 @@ export interface ILoginFormData {
 export interface handleLogin {
   handleLogin: (values: ILoginFormData) => void;
 }
+
+export interface AccountList {
+  assets: string;
+  broker_id: string;
+  created_at: string;
+  id: number;
+  is_active: boolean;
+  name: string;
+  number: string;
+  payments: string;
+  status: number;
+  updated_at: string;
+  user_id: number;
+  uuid: string;
+}
+
+export interface AccountProps {
+  totalCount: number;
+  accountList: AccountList[];
+}
