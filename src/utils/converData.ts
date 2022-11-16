3b618af010ff7a08dbcc687b3ea705394a6207d9
0SCAR0421 <oscar0421@kakao.com>
2022-11-16 23:35:53 +0900

    Feat: 시간에 대한 Util 함수 추가 작성

--- src/utils/converData.ts
+++ src/utils/converData.ts
@@ -1,7 +1,13 @@
 import accountStatus from "./accountStatus.json";
 
-export const convertAccountStatus = (status: number) => {
-  const result = Object.entries(accountStatus).filter((e) => e[1] === status);
-  if (result.length === 0) return "ERROR STATUS";
-  return result[0][0];
+export const convertDataFunction = {
+  convertAccountStatus: (status: number): string => {
+    const result = Object.entries(accountStatus).filter((e) => e[1] === status);
+    if (result.length === 0) return "ERROR STATUS";
+    return result[0][0];
+  },
+  convertCreatedDate: (date: string) => {
+    const newDate = new Date(date);
+    return newDate.toLocaleString();
+  },
 };
