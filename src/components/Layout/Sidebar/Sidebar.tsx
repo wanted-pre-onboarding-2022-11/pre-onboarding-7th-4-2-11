c2b17f9d0eed02a59bdcd86bcb372690d14a297a
0SCAR0421 <oscar0421@kakao.com>
2022-11-16 23:34:10 +0900

    Feat: Menu 클릭 시 Routing URL변경

--- src/components/Layout/Sidebar/Sidebar.tsx
+++ src/components/Layout/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SidebarMenu from "./SidebarMenu";
+import { useLocation } from "react-router-dom";
 
 interface SidebarProps {
   current: string | null;
@@ -7,14 +8,23 @@ interface SidebarProps {
   handleLogout(): void;
 }
 
+interface LocationProps {
+  state: {
+    page: string;
+  };
+}
+
 const Sidebar = ({ current, navigateTo, handleLogout }: SidebarProps) => {
+  const { state } = useLocation() as LocationProps;
+  const page = state?.page || "1";
+
   return (
     <div className="sticky top-0 left-0  p-10">
       <h1 className="text-6xl mb-16">PREFACE</h1>
       <SidebarMenu
         name="계좌목록"
         isSelected={current === "account"}
-        callback={() => navigateTo(`/?current=account`)}
+        callback={() => navigateTo(`/?current=account&page=${page}`)}
       />
       <SidebarMenu name="로그아웃" isSelected={current === "logout"} callback={handleLogout} />
     </div>
