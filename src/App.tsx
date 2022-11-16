61d99bbec5233bc5cfa48462af785ede991cb852
0SCAR0421 <oscar0421@kakao.com>
2022-11-16 23:34:40 +0900

    Feat: react-query 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import AppRouter from "./routes/AppRouter";
-import { FetchDataAPI } from "./apis/";
-
-export const fetchDataAPI = new FetchDataAPI();
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  return <AppRouter></AppRouter>;
+  const client = new QueryClient();
+  return (
+    <QueryClientProvider client={client}>
+      <AppRouter />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
