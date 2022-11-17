1831762ab436f19c8027bfeabb4a55161316525b
0SCAR0421 <oscar0421@kakao.com>
2022-11-17 14:00:12 +0900

    Feat: 데이터 포메팅 변경

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -15,7 +15,7 @@ const Table = ({ accountList, handleDetail }: TableProps) => {
         <thead>
           <tr>
             <th>고객명</th>
-            <th>브로커명</th>
+            <th>증권사</th>
             <th>계좌번호</th>
             <th>계좌상태</th>
             <th>계좌명</th>
@@ -28,13 +28,15 @@ const Table = ({ accountList, handleDetail }: TableProps) => {
         <tbody>
           {accountList.map((e) => (
             <tr key={e.uuid}>
-              <td onClick={() => handleDetail(e.uuid)}>{e.user_id}</td>
-              <td>{e.broker_id}</td>
-              <td>{e.number}</td>
+              <td>{convertDataFunction.convertAccountUserName(String(e.user_id))}</td>
+              <td>{convertDataFunction.convertAccountBroker(e.broker_id)}</td>
+              <td onClick={() => handleDetail(e.uuid)}>
+                {convertDataFunction.convertAccountNumber(e.number)}
+              </td>
               <td>{convertDataFunction.convertAccountStatus(e.status)}</td>
               <td>{e.name}</td>
-              <td>{e.assets}</td>
-              <td>{e.payments}</td>
+              <td>{convertDataFunction.convertAccountAssets(e.assets)} 원</td>
+              <td>{convertDataFunction.convertAccountAssets(e.payments)} 원</td>
               <td>{e.is_active ? "활성" : "비활성"}</td>
               <td>{convertDataFunction.convertCreatedDate(e.created_at)}</td>
             </tr>
@@ -69,6 +71,15 @@ const TableContainer = styled.div`
 
       & > tr > td {
         padding: 15px;
+
+        &:nth-child(3) {
+          cursor: pointer;
+          color: royalblue;
+
+          &:hover {
+            color: #000;
+          }
+        }
       }
     }
   }
--- src/utils/converData.ts
+++ src/utils/converData.ts
@@ -1,4 +1,5 @@
 import accountStatus from "./accountStatus.json";
+import brokers from "./brokers.json";
 
 export const convertDataFunction = {
   convertAccountStatus: (status: number): string => {
@@ -6,8 +7,40 @@ export const convertDataFunction = {
     if (result.length === 0) return "ERROR STATUS";
     return result[0][0];
   },
-  convertCreatedDate: (date: string) => {
+  convertCreatedDate: (date: string): string => {
     const newDate = new Date(date);
     return newDate.toLocaleString();
   },
+  convertAccountBroker: (broker: string): string => {
+    const brokersName: { [index: string]: string } = brokers;
+    return brokersName[broker];
+  },
+  convertAccountNumber: (number: string): string => {
+    return number.replace(/(?<=.{2})(?=.{3})./gi, "*");
+  },
+  convertAccountUserName: (name: string): string => {
+    const targetLength: number = name.length;
+    if (targetLength <= 2) return name.replace(/(?<=.{1})./gi, "*");
+    else return name.replace(/(?<=.{1})(?=.{2})./gi, "*");
+  },
+  convertAccountAssets: (assets: string): string => {
+    return Math.floor(Number(assets)).toLocaleString();
+  },
+};
+
+export const convertTitle = (title: string | null): string => {
+  let newTitle = "";
+
+  switch (title) {
+    case "account":
+      newTitle = "계좌목록";
+      break;
+    case "detail":
+      newTitle = "계좌상세";
+      break;
+    default:
+      newTitle = "";
+  }
+
+  return newTitle;
 };
