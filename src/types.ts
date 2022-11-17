fd8c0d4b7b41394c197c0de53cadb13f90174352
0SCAR0421 <oscar0421@kakao.com>
2022-11-17 14:01:57 +0900

    Docs: Interface 추가

--- src/types.ts
+++ src/types.ts
@@ -18,11 +18,43 @@ export interface AccountList {
   payments: string;
   status: number;
   updated_at: string;
-  user_id: number;
+  user_id: number | number;
   uuid: string;
 }
 
 export interface AccountProps {
   totalCount: number;
   accountList: AccountList[];
+}
+
+export interface UserList {
+  address: string;
+  age: number;
+  birth_date: string;
+  created_at: string;
+  detail_address: string;
+  email: string;
+  gender_origin: number;
+  id: number;
+  last_login: string;
+  name: string;
+  phone_number: string;
+  photo: string;
+  updated_at: string;
+  uuid: string;
+}
+
+export interface PatchAccountData {
+  assets?: string;
+  broker_id?: string;
+  created_at?: string;
+  id?: number;
+  is_active?: boolean;
+  name?: string;
+  number?: string;
+  payments?: string;
+  status?: number;
+  updated_at?: string;
+  user_id?: number;
+  uuid?: string;
 }
